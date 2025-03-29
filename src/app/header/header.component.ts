b3c0ee0a8866cbcbae450cb6bfeb99cbc8290ebd
aziztheiri <alatheiri123@gmail.com>
2025-03-29 01:51:45 +0100

    resp

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('hamburger') hamburger!: ElementRef;
+  @ViewChild('navList') navList!: ElementRef;
 
+  ngAfterViewInit() {
+    this.hamburger.nativeElement.addEventListener('click', () => {
+      this.toggleMenu();
+    });
+  }
+
+  toggleMenu() {
+    this.hamburger.nativeElement.classList.toggle('active');
+    this.navList.nativeElement.classList.toggle('active');
+  }
 }
